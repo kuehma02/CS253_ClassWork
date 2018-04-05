4239a7a37831e8501bf4e3dded3d3d89f8fed7eb
Madeline <kuehma02@luther.edu>
2018-04-05 14:05:34 -0500

    Started exercise 1

--- exercise1/src/exercise1.cpp
+++ exercise1/src/exercise1.cpp
@@ -2,23 +2,46 @@
 
 //Print "Hello World" to the default system output
 void task1() {    
-    //TODO: Implement task1
+    cout << "Hello World" << endl;
 }
 //Ask the user to enter their first name and print "Hello USERNAME"
 void task2() {
-    //TODO: Implement task2
+    string firstname;
+    cout << "Enter your first name" << endl;
+    cin >> firstname;
+    cout << "Hello " << firstname << endl;
 }
 //Ask a user to enter their full name and print the initials (ie. John Doe should yield JD)
 void task3() {
-    //TODO: Implement task3
+    string firstname, lastname;
+    cout << "Enter your full name" << endl;
+    cin >> firstname >> lastname;
+    cout << "Your initials are " << firstname.at(0) << lastname.at(0) <<endl;
 }
 //Ask a user to enter their age and print "Minor" (if age is less than 21) or "Major" (if age is 21+)
 void task4() {
-    //TODO: Implement task4
+    int age;
+    cout << "Enter age" << endl;
+    cin >> age;
+    if (age < 21) {
+        cout << "Minor" << endl;
+    }
+    else {
+        cout << "Major" << endl;
+    }
 }
 //Generate a random number, print it, and print whether it's odd or even
 void task5() {
-    //TODO: Implement task5
+    long a = rand();
+    cout << a << endl;
+    int rem = a % 2;
+
+    if (rem == 0){
+        cout << "Even";
+    } else {
+        cout << "Odd";
+    }
+    cout << endl;
 }
 //Print all numbers between 1 and 20 (inclusive).
 void task6() {
@@ -30,7 +53,22 @@ void task7() {
 }
 //Calculate and print sum of all multiples of 253 between 1 and 2018 (inclusive).
 void task8() {
-    //TODO: Implement task8
+    // int sum = 0;
+    // for (int i = 1; i <= 2018; ++i) {
+    //     if (i % 253 == 0){
+    //         sum = sum + i;
+    //     }
+    // }
+    // cout << sum << endl;
+    int a = 20;
+    int b = 21;
+
+    if (a++ >= b) {
+        cout << a << "is greater than " << b;
+    } else {
+        cout << a << " is less than " << b;
+    }
+    cout << endl;
 }
 //Calculate and print sum and product of all numbers between 1 and 20 (inclusive). Hint: use a proper data type to hold a very large value of the product. 
 void task9() {
