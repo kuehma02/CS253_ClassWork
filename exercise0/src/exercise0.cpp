0140adbd97c6f03d4fa868034bcdc9cf84d6ec53
Madeline <kuehma02@luther.edu>
2018-04-09 12:29:59 -0500

    clean up

--- exercise0/src/exercise0.cpp
+++
@@ -1,7 +0,0 @@
-/*
- * This is a sample implementation file
- * Implementation file must include the header file
- */
-#include "exercise0.hpp"
-
-/* Return sum of two integers */
--- exercise1/test/exercise1_test.cpp
+++
@@ -1,6 +0,0 @@
-/* 
- * This is an actual test file
- */
-#include "catch.hpp"
-#include "../src/exercise1.cpp"
- 
