96f5b601c8098f9310b4cfa6b07c92f6b2135522
Madeline <kuehma02@luther.edu>
2018-04-17 14:09:44 -0500

    Structure Notes

--- notes/maddies_notes/structures/structures.hpp
+++ notes/maddies_notes/structures/structures.hpp
@@ -2,7 +2,12 @@
 
 using namespace std;
 
-//structure in C++
+//structure in C++, in this case, creating object of type book.
 struct Book {
+    string title;
+    string author;
+    int pages;
+    private:
+        double price;
 
 };
