4b643701e2439316b242642c4169fd875883e2dc
Madeline <kuehma02@luther.edu>
2018-04-20 23:05:24 -0500

    updates

--- notes/maddies_notes/structures/quiz.cpp
+++ notes/maddies_notes/structures/quiz.cpp
@@ -1,40 +1,92 @@
 #include "structures.hpp"
 
 int main() {
-    cout <<"Hello, \\\"CS253\"\\" << endl;
+    // cout <<"Hello, \\\"CS253\"\\" << endl;
 
 
-    double l = 2;
-    cout << l << " " << 1/l << l+1/l << endl;
+    // double l = 2;
+    // cout << l << " " << 1/l << l+1/l << endl;
 
 
-    for (int g = 0; g> 5; g++){
+    // for (int g = 0; g> 5; g++){
+    //     cout  << "Yoda";
+    // }
+
+    // bool q = false;
+    // while(!q){
+    //     cout << "hi";
+    //     q = true;
+    // }
+
+
+    // int arr11[10];
+    // cout << arr11[0];
+
+    // vector<int> vec(10);
+    // cout << endl << "at is" << vec.at(0)<< endl;
+    // cout << 1*0+2*1+3*2;
+
+
+    // cout << "\n\n\n";
+    // int arr[2][3];
+    // for (int i =0; i<2; i++){
+    //     for (int j =0; j<3; j++){
+    //         arr[i][j]= (i+1)*(j+1);
+    //     }
+    //     cout << arr[1];
+    // }
+    // int p = 1;
+    // bool q = false;
+    // while (!q){
+    //     p =p*3;
+    //     if (p > 10)
+    //     q = true;
+    //     cout << p << " ";
+
+
+    int b = 3;
+    if (b = 5){
+        cout << "April" << b;
+    } else {
+        cout << "NOT april" << b;
+    }
+
+    for (int g = 0; g < 5; g+=2){
         cout  << "Yoda";
     }
 
-    bool q = false;
-    while(!q){
-        cout << "hi";
-        q = true;
+    int a =10;
+
+    cout << endl << (a * 1 / a) << "    " << 1/a << endl;
+
+
+    int arr10[10];
+    cout  << arr10[0] << endl;
+
+    int arr1[1][2][3];
+    for (int i =0; i<1; i++){
+        for (int j =0; j<2; j++){
+            for (int k = 0; k < j; k++){
+                arr1[i][j][k] = 1*i+2*j+3*k;
+            }}}
+    cout << endl << endl<< arr1 [0][1][2];
+
+    vector<int> vec6(5);
+    for (int i = 0; i< vec6.size(); i++){vec6.at(i)=i+1;}
+    for (int i = 0; i< vec6.size()/2; i++){
+        vec6.at(i)=vec6.at(vec6.size()-i-1);
+
+    }
+
+    for (int i = 0; i< vec6.size(); i++){
+        cout << vec6.at(i)<<' ';
     }
 
 
-    int arr11[10];
-    cout << arr11[0];
+    int d4;
+    cout << endl;
+    cout << sizeof(d4);
 
-    vector<int> vec(10);
-    cout << endl << "at is" << vec.at(0)<< endl;
-    cout << 1*0+2*1+3*2;
-
-
-    cout << "\n\n\n";
-    int arr[2][3];
-    for (int i =0; i<2; i++){
-        for (int j =0; j<3; j++){
-            arr[i][j]= (i+1)*(j+1);
-        }
-        cout << arr[1];
-    }
 
     return 0;
 }
